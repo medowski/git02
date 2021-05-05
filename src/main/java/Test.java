4579cf6d345dee10e59afe45c2d70d5fbfad393f
widmo <widmo@widmo.pl>
2021-05-06 00:58:46 +0200

    Commit 12345

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
         System.out.println("1");
         System.out.println("2");
+        System.out.println("3");
         System.out.println("4");
         System.out.println("5");
 
